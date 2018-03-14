cdbd2bab0a1055c4eed2ec2f0126f1ff1d22ddff
shawn <liushawn@outlook.com>
2018-03-14 18:25:10 +0800

    bean

---
+++ src/tmall/bean/ProductImage.java
@@ -0,0 +1,24 @@
+package tmall.bean;
+
+public class ProductImage {
+
+	private String type;
+	private Product product;
+	private int id;
+	
+	public String getType(){
+		return type;
+	}
+	
+	public void setType(String type){
+		this.type = type;
+	}
+	
+	public Product getProduct(){
+		return product;
+	}
+	
+	public void setProduct(Product product){
+		this.product = product;
+	}
+}
